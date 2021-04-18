fd599e805cce6da409fecbfeb0363edba06389ea
Eren Arı <erenari27@gmail.com>
2021-04-18 18:14:05 +0300

    dene

--- src/Navi.js
+++ src/Navi.js
@@ -19,7 +19,7 @@ const Navi = (props) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Northwind App</NavbarBrand>
+        <NavbarBrand href="/">Northwind </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
